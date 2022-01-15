054e138734b82efc4e186a13c78b144c6e6435ea
Sumeet sapla <s.sapla132@gmail.com>
2022-01-15 06:33:00 +0530

    Time: 4 ms (73.59%), Space: 40 MB (90.10%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
@@ -1,4 +1,23 @@
 class Solution {
+    
+     // Start from Last index of num Array
+        /*
+                          INDEX -> 0 1 2
+        Explanation: let  num --> [2,3,2]
+                          k   -->  8947
+                     ADD num[LAST INDEX] + k  --> 2 + 8947 =  8949 --> k'   ADD(9) to LIST
+                     Now ADD num[1] + (k' % 10) --> 3 + 894 = 897   --> k'   ADD(7) to LIST
+                     Now ADD num[0] + (k' % 10) --> 2 + 89 = 91     --> k'   ADD(1) to LIST
+                      k = k' % 10 = 9
+                     At the End of the loop k != 0    So  
+                     Now ADD DIGITS of k to list FROM the END     --> ADD(9) to List
+                     Now k is 0 so END while Loop
+                     
+       At this point we have List as --> [9,7,1,9]  --> **Here NOTE that it is the REQUIRED ANS BUT **
+	   IN REVERSE ORDER
+                     --> So, now REVERSE the LIST 
+                     --> At the END return the Required List           
+         */
     public List<Integer> addToArrayForm(int[] num, int k) {
         
      List<Integer> res = new ArrayList<>();
