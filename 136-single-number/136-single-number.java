fb5dde3d6371fc51c5141b2d5738a7c06b1642c0
Sumeet sapla <s.sapla132@gmail.com>
2022-03-10 22:36:25 +0530

    Time: 2 ms (58.19%), Space: 50.1 MB (60.66%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int result = 0;
+        
+        for(int i : nums)
+        {
+            result ^= i;
+        }
+        
+        return result;
+    }
+}
