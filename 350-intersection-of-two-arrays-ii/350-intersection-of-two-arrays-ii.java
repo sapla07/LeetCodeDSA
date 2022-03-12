4726eebb6f728aa4535953ece2a2c4a9dafb21c8
Sumeet sapla <s.sapla132@gmail.com>
2022-03-13 00:13:26 +0530

    Time: 1 ms (99.46%), Space: 44.2 MB (26.38%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {        
                
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> hm = new HashMap<>();
         
-       for(int i : nums1) hm.put(i, hm.getOrDefault(i,0)+1);
-       for(int i : nums2)
-       {
-           if(hm.getOrDefault(i,0) != 0)
-           {
-               list.add(i);
-               hm.put(i,hm.getOrDefault(i,1)-1);
-           }
-       }
-        
-        int[] arr = new int[list.size()];
-        for(int i=0; i < list.size(); i++)
-        {
-            arr[i] = list.get(i);
+    int map[] = new int[1001];
+    int res[] = new int[1001];
+    int count = 0;
+    for(int i:nums1)
+        map[i]++;
+    for(int i:nums2){
+        if(map[i]>0){
+            res[count++]=i;
+            map[i]--;
         }
+    }
+    return Arrays.copyOfRange(res,0,count);
 
-       
-        return arr;
     }
 }
