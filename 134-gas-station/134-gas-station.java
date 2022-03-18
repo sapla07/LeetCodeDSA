60b75f17b4ea2020c8dbe284e75074f7546e4aae
Sumeet sapla <s.sapla132@gmail.com>
2022-03-19 01:09:50 +0530

    Time: 1 ms (99.97%), Space: 61.8 MB (81.74%) - LeetHub

---
+++ 134-gas-station/134-gas-station.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int tank = 0;
+        for(int i=0; i < gas.length; i++){
+            tank += gas[i] - cost[i];
+        }
+        
+        if(tank < 0)
+            return -1;
+        
+        int start=0;
+        int accumulate = 0;
+        for(int i=0; i < gas.length; i++)
+        {
+
+            int currgain = gas[i]-cost[i];
+            
+            if(accumulate+currgain < 0){
+                start = i+1;
+                accumulate = 0;
+            }
+            else
+                accumulate += currgain;
+            
+        }
+        
+        return start;
+    }
+}
