279ca3f05e6eaefda118a25bc68d82df92b92850
Sumeet sapla <s.sapla132@gmail.com>
2022-03-26 18:12:50 +0530

    Time: 46 ms (5.20%), Space: 76.8 MB (5.27%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+                       
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i : nums)
+        {
+            if(hm.containsKey(i))
+                hm.put(i,hm.get(i)+1);
+            else
+                hm.put(i,1);
+        }
+        
+        for(int i : hm.values())
+        {
+            if( i >= 2)
+                return true;
+        }
+        
+        return false;
+    }
+}
